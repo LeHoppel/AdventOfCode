17301f119e38b1e53c6b9e9927a3df0307fdb97a
Lennart Haase <lennarthaase@gmx.de>
2024-11-25 21:13:08 +0100

    Solved Day01First of 2023.

---
+++ AdventOfCode/2023/Day01/Day01First.cs
@@ -0,0 +1,26 @@
+﻿namespace AdventOfCode._2023.Day01;
+
+public static class Day01First
+{
+    public static int CalculateAnswer()
+    {
+        StreamReader streamReader = new StreamReader("C:\\Users\\Lennart\\RiderProjects\\AdventOfCode\\AdventOfCode\\2023\\Day01\\input.txt");
+        string? currentLine = streamReader.ReadLine(); 
+        int answerValue = 0;
+
+        while (currentLine != null)
+        {
+            List<char> filteredLine = currentLine.Where(c => char.IsDigit(c)).ToList();
+            
+            
+            if (filteredLine.Count == 1)
+                answerValue += int.Parse($"{filteredLine.First()}{filteredLine.First()}");
+            if (filteredLine.Count >= 2)
+                answerValue += int.Parse($"{filteredLine.First()}{filteredLine.Last()}");
+
+            currentLine = streamReader.ReadLine(); 
+        }
+
+        return answerValue;
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AdventOfCode._2023.Day01;
+
+Console.WriteLine($"Day 01 of 2023: {Day01First.CalculateAnswer()}");
