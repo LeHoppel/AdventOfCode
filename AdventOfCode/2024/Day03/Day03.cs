02023209c4f484c8a8b8e785d9702ec8cc03a0fd
Lennart Haase <lennart.leander.haase@iml.fraunhofer.de>
2024-12-02 13:38:22 +0100

    Added code frame for day 03.

---
+++ AdventOfCode/2024/Day03/Day03.cs
@@ -0,0 +1,36 @@
+namespace AdventOfCode._2024.Day03;
+
+public static class Day03
+{
+    public static int CalculatePart01()
+    {
+        StreamReader streamReader = new StreamReader("D:\\Lennart\\Git\\AdventOfCode\\AdventOfCode\\2024\\Day03\\input.txt");
+        string? currentLine = streamReader.ReadLine(); 
+
+        while (currentLine != null)
+        {
+            
+            currentLine = streamReader.ReadLine(); 
+        }
+       
+        int answerValue = 0;
+        
+        return answerValue;
+    }
+
+    public static int CalculatePart02()
+    {
+        StreamReader streamReader = new StreamReader("D:\\Lennart\\Git\\AdventOfCode\\AdventOfCode\\2024\\Day03\\input.txt");
+        string? currentLine = streamReader.ReadLine(); 
+
+        while (currentLine != null)
+        {
+            
+            currentLine = streamReader.ReadLine(); 
+        }
+       
+        int answerValue = 0;
+        
+        return answerValue;
+    }
+}
