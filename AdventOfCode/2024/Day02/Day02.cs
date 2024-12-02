ae2772048bb8fdf63369c5010de07b7c56e44c27
Lennart Haase <lennart.leander.haase@iml.fraunhofer.de>
2024-12-02 11:56:03 +0100

    Solved Day02 part 2.

--- AdventOfCode/2024/Day02/Day02.cs
+++ AdventOfCode/2024/Day02/Day02.cs
@@ -2,7 +2,7 @@ namespace AdventOfCode._2024.Day02;
 
 public static class Day02
 {
-    public static int CalculateAnswer()
+    public static int CalculatePart01()
     {
         StreamReader streamReader = new StreamReader("D:\\Lennart\\Git\\AdventOfCode\\AdventOfCode\\2024\\Day02\\input.txt");
         string? currentLine = streamReader.ReadLine(); 
@@ -17,7 +17,7 @@ public static class Day02
 
             bool lineIsSafe = false;
             
-            lineIsSafe = lineNumbers[0] < lineNumbers[1] ? CheckSafetyAscending(lineNumbers) : CheckSafetyDescending(lineNumbers);
+            lineIsSafe = CheckSafety(lineNumbers);
 
             if (lineIsSafe) answerValue++;
             
@@ -27,23 +27,55 @@ public static class Day02
         return answerValue;
     }
 
-    private static bool CheckSafetyAscending(List<int> lineNumbers)
+    public static int CalculatePart02()
     {
-        for (int i = 0; i < lineNumbers.Count - 1; i++)
+        StreamReader streamReader = new StreamReader("D:\\Lennart\\Git\\AdventOfCode\\AdventOfCode\\2024\\Day02\\input.txt");
+        string? currentLine = streamReader.ReadLine(); 
+        int answerValue = 0;
+
+        while (currentLine != null)
         {
-            if (lineNumbers[i] > lineNumbers[i + 1]) return false;
-            int difference = int.Abs(lineNumbers[i] - lineNumbers[i + 1]);
-            if (difference < 1 || difference > 3) return false;
+            List<int> lineNumbers = new List<int>();
+            
+            foreach (string linePart in currentLine.Split(" "))
+                lineNumbers.Add(int.Parse(linePart));
+            
+            if (CheckSafety(lineNumbers))
+            {
+                answerValue++;
+                currentLine = streamReader.ReadLine(); 
+                continue;
+            }
+
+            for (int i = 0; i < lineNumbers.Count; i++)
+            {
+                List<int> subList = new List<int>(lineNumbers);
+                subList.RemoveAt(i);
+                
+                if (CheckSafety(subList))
+                {
+                    answerValue++;
+                    break;
+                }
+            }
+            currentLine = streamReader.ReadLine(); 
         }
 
-        return true;
+        return answerValue;
     }
-    
-    private static bool CheckSafetyDescending(List<int> lineNumbers)
+
+    private static bool CheckSafety(List<int> lineNumbers)
     {
+        Func<int, int, bool> comparison;
+
+        if (lineNumbers[0] < lineNumbers[1])
+            comparison = (x, y) => x < y;
+        else
+            comparison = (x, y) => x > y;
+        
         for (int i = 0; i < lineNumbers.Count - 1; i++)
         {
-            if (lineNumbers[i] < lineNumbers[i + 1]) return false;
+            if (!comparison(lineNumbers[i],  lineNumbers[i + 1])) return false;
             int difference = int.Abs(lineNumbers[i] - lineNumbers[i + 1]);
             if (difference < 1 || difference > 3) return false;
         }
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,3 +1,3 @@
 ﻿using AdventOfCode._2024.Day02;
 
-Console.WriteLine($"Day 02 of 2024: {Day02.CalculateAnswer()}");
+Console.WriteLine($"Day 02 of 2024 Part01: {Day02.CalculatePart01()}, Part02: {Day02.CalculatePart02()}");
