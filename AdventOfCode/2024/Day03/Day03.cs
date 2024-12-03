6e47c44d447e3bac24cf62ed4383076835efffbf
Lennart Haase <lennarthaase@gmx.de>
2024-12-03 09:16:47 +0100

    Solved second part of day 03.

--- AdventOfCode/2024/Day03/Day03.cs
+++ AdventOfCode/2024/Day03/Day03.cs
@@ -13,14 +13,7 @@ public static class Day03
         
         while (currentLine != null)
         {
-            MatchCollection match = Regex.Matches(currentLine, "(mul\\()[0-9]+(,)[0-9]+(\\))");
-            foreach (Capture capture in match)
-            {
-                string[] splitEquation = capture.Value.Split(["mul(", ",", ")"], StringSplitOptions.RemoveEmptyEntries);
-                int a = int.Parse(splitEquation[0]);
-                int b = int.Parse(splitEquation[1]);
-                answerValue += a * b;
-            }
+            answerValue += EvaluateMuls(currentLine);
             
             currentLine = streamReader.ReadLine();
         }
@@ -28,18 +21,41 @@ public static class Day03
         return answerValue;
     }
 
+    private static int EvaluateMuls(string input)
+    {
+        int value = 0;
+        
+        MatchCollection match = Regex.Matches(input, "(mul\\()[0-9]+(,)[0-9]+(\\))");
+        foreach (Capture capture in match)
+        {
+            string[] splitEquation = capture.Value.Split(["mul(", ",", ")"], StringSplitOptions.RemoveEmptyEntries);
+            int a = int.Parse(splitEquation[0]);
+            int b = int.Parse(splitEquation[1]);
+            value += a * b;
+        }
+
+        return value;
+    }
+
     public static int CalculatePart02()
     {
-        StreamReader streamReader = new StreamReader("C:\\Users\\Lennart\\RiderProjects\\AdventOfCode\\AdventOfCode\\2024\\Day03\\example.txt");
+        StreamReader streamReader = new StreamReader("C:\\Users\\Lennart\\RiderProjects\\AdventOfCode\\AdventOfCode\\2024\\Day03\\input.txt");
         string? currentLine = streamReader.ReadLine(); 
 
+        int answerValue = 0;
+        
         while (currentLine != null)
         {
+            string[] splitLine = Regex.Split(currentLine, @"(?=do\(\)|don't\(\))");
             
+            foreach (string s in splitLine)
+            {
+                if (!s.StartsWith("don't()"))
+                    answerValue += EvaluateMuls(s);
+            }
+
             currentLine = streamReader.ReadLine(); 
         }
-       
-        int answerValue = 0;
         
         return answerValue;
     }
