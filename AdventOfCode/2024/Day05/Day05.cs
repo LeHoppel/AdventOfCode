9ca092a5a9e4b2efbee6630503667a3020ac30a6
Lennart Haase <lennarthaase@gmx.de>
2024-12-05 10:12:48 +0100

    Solved Day05 (a bit messy).

--- AdventOfCode/2024/Day05/Day05.cs
+++ AdventOfCode/2024/Day05/Day05.cs
@@ -7,6 +7,59 @@ public class Day05 : Day
         List<string> input = ReadInput(pathPrefix + "\\" + kindOfInput + ".txt");
         int answerValue = 0;
 
+        Dictionary<int,List<int>> pageOrderingRules = new Dictionary<int,List<int>>();
+        int numberOfRules = 0;
+        
+        foreach (string line in input)
+        {
+            if (!line.Contains('|')) break;
+            
+            string[] lineParts = line.Split("|");
+            int left  = int.Parse(lineParts[0]);
+            int right = int.Parse(lineParts[1]);
+
+
+            if (!pageOrderingRules.ContainsKey(left))
+            {
+                var rules = new List<int>();
+                rules.Add(right);
+                pageOrderingRules.Add(left, rules);
+            }
+            else
+                pageOrderingRules[left].Add(right);
+
+            numberOfRules++;
+        }
+        
+        input.RemoveRange(0, numberOfRules+1);
+
+        foreach (string line in input)
+        {
+            string[] lineParts = line.Split(",");
+            bool ruleBroken = false;
+
+            for (int i = 0; i < lineParts.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    int left = int.Parse(lineParts[i]);
+                    int right = int.Parse(lineParts[j]);
+                    
+                    if (pageOrderingRules.ContainsKey(left) && pageOrderingRules[left].Contains(right))
+                    {
+                        ruleBroken = true;
+                        break;
+                    }
+                }
+
+                if (ruleBroken)
+                    break;
+            }
+
+            if (!ruleBroken)
+                answerValue += int.Parse(lineParts[lineParts.Length/2]);
+        }
+        
         return answerValue;
     }
 
@@ -14,6 +67,62 @@ public class Day05 : Day
     {
         List<string> input = ReadInput(pathPrefix + "\\" + kindOfInput + ".txt");
         int answerValue = 0;
+
+        Dictionary<int,List<int>> pageOrderingRules = new Dictionary<int,List<int>>();
+        int numberOfRules = 0;
+        
+        foreach (string line in input)
+        {
+            if (!line.Contains('|')) break;
+            
+            string[] lineParts = line.Split("|");
+            int left  = int.Parse(lineParts[0]);
+            int right = int.Parse(lineParts[1]);
+
+
+            if (!pageOrderingRules.ContainsKey(left))
+            {
+                var rules = new List<int>();
+                rules.Add(right);
+                pageOrderingRules.Add(left, rules);
+            }
+            else
+                pageOrderingRules[left].Add(right);
+
+            numberOfRules++;
+        }
+        
+        input.RemoveRange(0, numberOfRules+1);
+
+        foreach (string line in input)
+        {
+            string[] lineParts = line.Split(",");
+            bool ruleBroken = false;
+
+            for (int i = 0; i < lineParts.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    int left = int.Parse(lineParts[i]);
+                    int right = int.Parse(lineParts[j]);
+                    
+                    if (pageOrderingRules.ContainsKey(left) && pageOrderingRules[left].Contains(right))
+                    {
+                        ruleBroken = true;
+                        break;
+                    }
+                }
+
+                if (ruleBroken)
+                {
+                    List<string> linePartsList = lineParts.ToList();
+                    linePartsList.Sort( (x,y) => pageOrderingRules.ContainsKey(int.Parse(x)) && pageOrderingRules[int.Parse(x)].Contains(int.Parse(y)) ? -1 : 1);
+                    
+                    answerValue += int.Parse(linePartsList[linePartsList.Count/2]);
+                    break;
+                }
+            }
+        }
         
         return answerValue;
     }
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -12,12 +12,15 @@ List<Day> days =
     new Day08()
 ];
 
-int startDay = 1;
-int endDay = 8;
+int startDay = 5;
+int endDay = 5;
 
 for (int i = startDay-1; i < endDay; i++)
 {
-    string pathPrefix = "D:\\Lennart\\Git\\AdventOfCode\\AdventOfCode\\2024\\Day" + (i+1 < 10 ? "0" + (i+1) : i+1);
+    // Work:
+    //string pathPrefix = "D:\\Lennart\\Git\\AdventOfCode\\AdventOfCode\\2024\\Day" + (i+1 < 10 ? "0" + (i+1) : i+1);
+    // Home:
+    string pathPrefix = "C:\\Users\\Lennart\\RiderProjects\\AdventOfCode\\AdventOfCode\\2024\\Day" + (i+1 < 10 ? "0" + (i+1) : i+1);
     
     Console.WriteLine($"Day {i+1} of 2024 Part01: example={days[i].CalculatePart01("example", pathPrefix)}, input={days[i].CalculatePart01("input", pathPrefix)}; " +
                       $"Part02: example={days[i].CalculatePart02("example", pathPrefix)}, input={days[i].CalculatePart02("input", pathPrefix)}");
