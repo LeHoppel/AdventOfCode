cf0f6f4a426fb4ef8625270edf7ad014ba28e3e6
Lennart Haase <lennarthaase@gmx.de>
2024-12-08 12:27:11 +0100

    Added code frames for day 09 to 15.

---
+++ AdventOfCode/2024/Day10/Day10.cs
@@ -0,0 +1,20 @@
+namespace AdventOfCode._2024;
+
+public class Day10 : Day
+{
+    public override int CalculatePart01(string kindOfInput, string pathPrefix)
+    {
+        List<string> input = ReadInput(pathPrefix + "\\" + kindOfInput + ".txt");
+        int answerValue = 0;
+
+        return answerValue;
+    }
+
+    public override int CalculatePart02(string kindOfInput, string pathPrefix)
+    {
+        List<string> input = ReadInput(pathPrefix + "\\" + kindOfInput + ".txt");
+        int answerValue = 0;
+        
+        return answerValue;
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -9,7 +9,14 @@ List<Day> days =
     new Day05(),
     new Day06(),
     new Day07(),
-    new Day08()
+    new Day08(),
+    new Day09(),
+    new Day10(),
+    new Day11(),
+    new Day12(),
+    new Day13(),
+    new Day14(),
+    new Day15()
 ];
 
 int startDay = 8;
