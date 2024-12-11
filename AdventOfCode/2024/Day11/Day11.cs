ed241955f8559d96be18760ca2b4e75ca2c41b25
Lennart Haase <lennart.leander.haase@iml.fraunhofer.de>
2024-12-11 13:06:59 +0100

    Solved second part of day 11.

--- AdventOfCode/2024/Day11/Day11.cs
+++ AdventOfCode/2024/Day11/Day11.cs
@@ -2,18 +2,16 @@ namespace AdventOfCode._2024;
 
 public class Day11 : Day
 {
-    public override bool PrintTime { get => false; set { } }
+    public override bool PrintTime { get => true; set { } }
     
     public override long CalculatePart01(string kindOfInput, string pathPrefix)
     {
-        List<string> input = ReadInput(pathPrefix + "\\" + kindOfInput + ".txt")[0].Split(" ").ToList();;
-        int answerValue = 0;
-        Console.WriteLine(string.Join(",", input));
+        List<string> input = ReadInput(pathPrefix + "\\" + kindOfInput + ".txt")[0].Split(" ").ToList();
         
         for (int i = 0; i < 25; i++)
             input = UpdateStones(input);
 
-        answerValue = input.Count;
+        int answerValue = input.Count;
 
         return answerValue;
     }
@@ -30,7 +28,7 @@ public class Day11 : Day
                 string secondHalf = input[i].Substring(input[i].Length/2, input[i].Length - firstHalf.Length);
    
                 input[i] = firstHalf;
-                input.Insert(i+1, int.Parse(secondHalf).ToString());
+                input.Insert(i, int.Parse(secondHalf).ToString());
                 i++;
             }
             else
@@ -40,11 +38,50 @@ public class Day11 : Day
         return input;
     }
     
+    private Dictionary<string, long> UpdateStoneTuples(Dictionary<string, long> input) 
+    {
+        Dictionary<string, long> temp = new();
+        
+        foreach (KeyValuePair<string,long> kvp in input)
+        {
+            if (kvp.Key == "0")
+            {
+                if (!temp.TryAdd("1", input[kvp.Key]))
+                    temp["1"] += input[kvp.Key];
+            }
+            else if (kvp.Key.Length % 2 == 0) 
+            {
+                string firstHalf = kvp.Key.Substring(0, kvp.Key.Length/2);
+                string secondHalf = kvp.Key.Substring(kvp.Key.Length/2, kvp.Key.Length - firstHalf.Length);
+                secondHalf = long.Parse(secondHalf).ToString();
+   
+                if (!temp.TryAdd(firstHalf, input[kvp.Key]))
+                    temp[firstHalf] += input[kvp.Key];
+                if (!temp.TryAdd(secondHalf, input[kvp.Key]))
+                    temp[secondHalf] += input[kvp.Key];
+            }
+            else
+            {
+                string parsed = (long.Parse(kvp.Key) * 2024).ToString();
+                if (!temp.TryAdd(parsed, input[kvp.Key]))
+                    temp[parsed] += input[kvp.Key];
+            }
+        }
+ 
+        return temp;
+    }
+    
     public override long CalculatePart02(string kindOfInput, string pathPrefix)
     {
-        List<string> input = ReadInput(pathPrefix + "\\" + kindOfInput + ".txt");
-        int answerValue = 0;
+        List<string> inputStrings = ReadInput(pathPrefix + "\\" + kindOfInput + ".txt")[0].Split(" ").ToList();
+        Dictionary<string, long> input = new();
+
+        foreach (string inputString in inputStrings)
+            input.Add(inputString, 1);
         
-        return answerValue;
+        for (int i = 0; i < 75; i++)
+            input = UpdateStoneTuples(input);
+
+        return input.Values.Sum();
     }
 }
