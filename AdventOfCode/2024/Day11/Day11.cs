b85796bda04a895f9ea2ce9201e9bf9ad80631a7
Lennart Haase <lennart.leander.haase@iml.fraunhofer.de>
2024-12-11 09:15:09 +0100

    Solved first part of day 11.

--- AdventOfCode/2024/Day11/Day11.cs
+++ AdventOfCode/2024/Day11/Day11.cs
@@ -6,12 +6,40 @@ public class Day11 : Day
     
     public override long CalculatePart01(string kindOfInput, string pathPrefix)
     {
-        List<string> input = ReadInput(pathPrefix + "\\" + kindOfInput + ".txt");
+        List<string> input = ReadInput(pathPrefix + "\\" + kindOfInput + ".txt")[0].Split(" ").ToList();;
         int answerValue = 0;
+        Console.WriteLine(string.Join(",", input));
+        
+        for (int i = 0; i < 25; i++)
+            input = UpdateStones(input);
+
+        answerValue = input.Count;
 
         return answerValue;
     }
 
+    private List<string> UpdateStones(List<string> input) 
+    {
+        for (int i = 0; i < input.Count; i++)
+        {  
+            if (input[i] == "0")
+                input[i] = "1";
+            else if (input[i].Length % 2 == 0) 
+            {
+                string firstHalf = input[i].Substring(0, input[i].Length/2);
+                string secondHalf = input[i].Substring(input[i].Length/2, input[i].Length - firstHalf.Length);
+   
+                input[i] = firstHalf;
+                input.Insert(i+1, int.Parse(secondHalf).ToString());
+                i++;
+            }
+            else
+                input[i] = (long.Parse(input[i]) * 2024).ToString();
+        }
+ 
+        return input;
+    }
+    
     public override long CalculatePart02(string kindOfInput, string pathPrefix)
     {
         List<string> input = ReadInput(pathPrefix + "\\" + kindOfInput + ".txt");
